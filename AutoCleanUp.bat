8748a9b47a7edc6cec8b432c9df61ca5dd9a3f02
frex <79519721+frex777@users.noreply.github.com>
2025-11-21 17:38:36 +0100

    Update AutoCleanUp.bat
    Fixed version for 25H2 and other versions.

--- AutoCleanUp.bat
+++ AutoCleanUp.bat
@@ -1,47 +1,29 @@
 @echo off
-color 0A
+color 0B
+echo [Maintenance]: Cleaning up the trash...
 
 net session >nul 2>&1
 if %errorLevel% NEQ 0 (
-    echo [AutoCleanUp]: Running with administrator privileges...
     powershell -Command "Start-Process cmd -ArgumentList '/c %~s0' -Verb runAs"
     exit /b
 )
-
-echo [AutoCleanUp]: Cleaning temporary folders...
 
 del /s /f /q "%TEMP%\*" 2>nul
 del /s /f /q "%SystemRoot%\Temp\*" 2>nul
 del /s /f /q "%LOCALAPPDATA%\Temp\*" 2>nul
 
-del /s /f /q "%SystemRoot%\Prefetch\*" 2>nul
-
-del /s /f /q "%SystemRoot%\SoftwareDistribution\Download\*" 2>nul
-
 del /s /f /q "%LOCALAPPDATA%\Microsoft\Windows\WER\ReportQueue\*" 2>nul
 del /s /f /q "%LOCALAPPDATA%\Microsoft\Windows\WER\Temp\*" 2>nul
 
-:: ========================================================
-
-echo [AutoCleanUp]: Cleaning Windows Update leftovers...
-del /s /f /q "%SystemRoot%\SoftwareDistribution\Datastore\*" 2>nul
-del /s /f /q "%SystemRoot%\SoftwareDistribution\DeliveryOptimization\*" 2>nul
-
-echo [AutoCleanUp]: Cleaning Windows logs...
 wevtutil cl Application
 wevtutil cl Security
 
-echo [AutoCleanUp]: Removing old drivers...
-pnputil /d e /f
-
-echo [AutoCleanUp]: Disabling and removing hibernation file...
-powercfg -h off
-
-echo [AutoCleanUp]: Cleanup completed.
-
+echo [Maintenance]: Ready. Your PC will thank you :).
+timeout /t 3
 exit
 
 :: ========================================================
 :: AUTHOR: frex777
 :: ========================================================
+
 
