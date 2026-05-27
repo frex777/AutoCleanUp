af5d863f61a8bd1dddfe8a751928f235c5d7780f
frex <79519721+frex777@users.noreply.github.com>
2026-05-27 19:33:02 +0200

    Update AutoCleanUp.bat

--- AutoCleanUp.bat
+++ AutoCleanUp.bat
@@ -1,29 +1,17 @@
 @echo off
-color 0B
-echo [Maintenance]: Cleaning up the trash...
+del /s /f /q "%TEMP%\*.*" 2>nul
+for /d %%x in ("%TEMP%\*") do rmdir /s /q "%%x" 2>nul
 
-net session >nul 2>&1
-if %errorLevel% NEQ 0 (
-    powershell -Command "Start-Process cmd -ArgumentList '/c %~s0' -Verb runAs"
-    exit /b
-)
+del /s /f /q "%SystemRoot%\Temp\*.*" 2>nul
+for /d %%x in ("%SystemRoot%\Temp\*") do rmdir /s /q "%%x" 2>nul
 
-del /s /f /q "%TEMP%\*" 2>nul
-del /s /f /q "%SystemRoot%\Temp\*" 2>nul
-del /s /f /q "%LOCALAPPDATA%\Temp\*" 2>nul
-
-del /s /f /q "%LOCALAPPDATA%\Microsoft\Windows\WER\ReportQueue\*" 2>nul
-del /s /f /q "%LOCALAPPDATA%\Microsoft\Windows\WER\Temp\*" 2>nul
-
-wevtutil cl Application
-wevtutil cl Security
-
-echo [Maintenance]: Ready. Your PC will thank you :).
-exit
+del /s /f /q "%LOCALAPPDATA%\Microsoft\Windows\WER\ReportQueue\*.*" 2>nul
+del /s /f /q "%LOCALAPPDATA%\Microsoft\Windows\WER\Temp\*.*" 2>nul
 
 :: ========================================================
 :: AUTHOR: frex777
 :: ========================================================
+exit
 
 
 
